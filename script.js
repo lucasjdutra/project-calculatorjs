0ee3df2c074edb9d7eda5a4b32c5be898991cce0
lucasjdutra <lucasjdc90@gmail.com>
2026-01-16 21:43:14 -0300

    closes #3 - Add sum operation

---
+++ script.js
@@ -0,0 +1,8 @@
+function sum(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    let output = num1 + num2;
+
+    document.getElementById("result").value = result;
+}
